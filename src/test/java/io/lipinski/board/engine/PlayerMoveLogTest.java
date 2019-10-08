d218d768ab9948dac4fe52f88902d5672fdcfff4
Pawel Lipinski <lipinskijob@gmail.com>
2019-10-08 21:06:27 +0200

    Make PlayerMoveLog more advance

--- src/test/java/io/lipinski/board/engine/PlayerMoveLogTest.java
+++ src/test/java/io/lipinski/board/engine/PlayerMoveLogTest.java
@@ -1,7 +1,10 @@
 package io.lipinski.board.engine;
 
 import org.assertj.core.api.Assertions;
+import org.assertj.core.util.Lists;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -9,49 +12,260 @@ import java.util.List;
 @DisplayName("Internal -- PlayerMoveLog")
 class PlayerMoveLogTest {
 
-    @Test
-    @DisplayName("Undo first move")
-    void undoFirstMove() {
-        final var moveLog = new PlayerMoveLog();
+    private PlayerMoveLog moveLog;
 
-        final var afterUndo = moveLog
-                .addMove(new Move(List.of(Direction.E, Direction.N)))
-                .undoMove();
-
-        Assertions.assertThat(afterUndo).isEqualToComparingFieldByField(moveLog);
+    @BeforeEach
+    void setUp() {
+        moveLog = new PlayerMoveLog();
     }
 
-    @Test
-    @DisplayName("Undo second move")
-    void undoSecondMove() {
-        final var oneMove = new PlayerMoveLog()
-                .addMove(new Move(List.of(Direction.E, Direction.N)));
+    @Nested
+    @DisplayName("sanity -- recursively")
+    class SanityTest {
 
-        final var afterMoveAndUndo = oneMove
-                .addMove(new Move(List.of(Direction.NE, Direction.W)))
-                .undoMove();
+        @Test
+        @DisplayName("one move, one direction, one undo")
+        void oneMoveOneDirectionOneUndo() {
+            final var afterMoves = moveLog
+                    .addMove(new Move(List.of(Direction.S)))
+                    .add(Direction.N)
+                    .undoMove();
 
-        Assertions.assertThat(afterMoveAndUndo).isEqualToComparingFieldByField(oneMove);
+            Assertions.assertThat(afterMoves).isEqualToComparingFieldByFieldRecursively(moveLog);
+        }
     }
 
-    @Test
-    @DisplayName("Check player 2 moves")
-    void checkPlayer() {
-        final var moveLog = new PlayerMoveLog()
-                .addMove(new Move(List.of(Direction.NE)))
-                .addMove(new Move(List.of(Direction.N)));
+    @Nested
+    @DisplayName("undo")
+    class UndoMove {
 
-        Assertions.assertThat(moveLog.currentPlayer()).isEqualByComparingTo(Player.FIRST);
+        @Test
+        @DisplayName("zero direction, zero moves, one undo")
+        void zeroDirectionsZeroMovesOneUndo() {
+            final var undo = moveLog.undo();
+
+            Assertions.assertThat(undo).isEqualToComparingFieldByFieldRecursively(moveLog);
+        }
+
+        @Test
+        @DisplayName("two direction, one undo")
+        void twoDirectionsOneUndo() {
+            final var afterUndo = moveLog
+                    .add(Direction.N)
+                    .add(Direction.W)
+                    .undo();
+
+            Assertions.assertThat(afterUndo.allDirections()).containsExactlyElementsOf(List.of(Direction.N));
+        }
+
+        @Test
+        @DisplayName("one move, one undo")
+        void oneMoveOneUndo() {
+            final var afterMoves = moveLog
+                    .addMove(new Move(List.of(Direction.S, Direction.E)))
+                    .undo();
+
+            Assertions.assertThat(afterMoves.allDirections()).containsExactlyElementsOf(List.of(Direction.S, Direction.E));
+        }
+
+        @Test
+        @DisplayName("one move, one direction, one undo")
+        void oneMoveOneDirectionOneUndo() {
+            final var afterMoves = moveLog
+                    .addMove(new Move(List.of(Direction.S)))
+                    .add(Direction.W)
+                    .undo();
+
+            Assertions.assertThat(afterMoves.allDirections()).containsExactlyElementsOf(List.of(Direction.S));
+        }
+
+        @Test
+        @DisplayName("one move, one direction, one move, one undo")
+        void oneMoveOneDirectionOneMoveOneUndo() {
+            final var afterMoves = moveLog
+                    .addMove(new Move(List.of(Direction.S)))
+                    .add(Direction.W)
+                    .addMove(new Move(List.of(Direction.S)))
+                    .undo();
+
+            Assertions.assertThat(afterMoves.allDirections()).containsExactlyElementsOf(List.of(Direction.S, Direction.W, Direction.S));
+        }
     }
 
-    @Test
-    @DisplayName("Check player 2 moves 1 undo")
-    void checkPlayerUndo() {
-        final var moveLog = new PlayerMoveLog()
-                .addMove(new Move(List.of(Direction.NE)))
-                .addMove(new Move(List.of(Direction.N)))
-                .undoMove();
+    @Nested
+    @DisplayName("allDirections")
+    class AllDirectionsTest {
 
-        Assertions.assertThat(moveLog.currentPlayer()).isEqualByComparingTo(Player.SECOND);
+        @Test
+        @DisplayName("0 move, 0 directions")
+        void zeroMovesAndDirections() {
+            Assertions.assertThat(moveLog.allDirections()).isEqualTo(Lists.emptyList());
+        }
+
+        @Test
+        @DisplayName("two moves, the same order")
+        void twoMovesTheSameOrder() {
+            final var prepared = List.of(Direction.NE, Direction.N);
+
+            final var afterMoves = moveLog
+                    .addMove(new Move(List.of(Direction.NE)))
+                    .addMove(new Move(List.of(Direction.N)));
+
+            Assertions.assertThat(afterMoves.allDirections()).containsExactlyElementsOf(prepared);
+        }
+
+        @Test
+        @DisplayName("two moves, one direction, the same order")
+        void twoMovesOneDirectionTheSameOrder() {
+            final var prepared = List.of(Direction.N, Direction.W, Direction.SE);
+
+            final var afterMoves = moveLog
+                    .addMove(new Move(List.of(Direction.N)))
+                    .addMove(new Move(List.of(Direction.W)))
+                    .add(Direction.SE);
+
+            Assertions.assertThat(afterMoves.allDirections()).containsExactlyElementsOf(prepared);
+        }
+
+        @Test
+        @DisplayName("two directions, the same order")
+        void twoDirectionsTheSameOrder() {
+            final var prepared = List.of(Direction.N, Direction.W);
+            final var afterMoves = moveLog
+                    .add(Direction.N)
+                    .add(Direction.W);
+
+            Assertions.assertThat(afterMoves.allDirections()).containsExactlyElementsOf(prepared);
+        }
+
+        @Test
+        @DisplayName("two directions, one move, the same order")
+        void twoDirectionOneMoveTheSameOrder() {
+            final var prepared = List.of(Direction.W, Direction.N, Direction.S);
+            final var afterMoves = moveLog
+                    .add(Direction.W)
+                    .add(Direction.N)
+                    .addMove(new Move(List.of(Direction.S)));
+
+            Assertions.assertThat(afterMoves.allDirections()).containsExactlyElementsOf(prepared);
+        }
+
+        @Test
+        @DisplayName("one move, one direction, one move, the same order")
+        void oneMoveOneDirectionOneMoveTheSameOrder() {
+            final var prepared = List.of(Direction.S, Direction.W, Direction.S);
+            final var afterMoves = moveLog
+                    .addMove(new Move(List.of(Direction.S)))
+                    .add(Direction.W)
+                    .addMove(new Move(List.of(Direction.S)));
+
+            Assertions.assertThat(afterMoves.allDirections()).containsExactlyElementsOf(prepared);
+        }
+    }
+
+    @Nested
+    @DisplayName("currentPlayer")
+    class CurrentPLayerTest {
+
+        @Test
+        @DisplayName("one move")
+        void oneMove() {
+            final var afterMove = moveLog.addMove(new Move(List.of(Direction.NE)));
+
+            Assertions.assertThat(afterMove.currentPlayer()).isEqualByComparingTo(Player.SECOND);
+        }
+
+        @Test
+        @DisplayName("one direction")
+        void oneDirection() {
+            final var afterMove = moveLog.add(Direction.N);
+
+            Assertions.assertThat(afterMove.currentPlayer()).isEqualByComparingTo(Player.FIRST);
+        }
+
+        @Test
+        @DisplayName("two moves")
+        void twoMoves() {
+            final var afterMoves = moveLog
+                    .addMove(new Move(List.of(Direction.NE)))
+                    .addMove(new Move(List.of(Direction.N)));
+
+            Assertions.assertThat(afterMoves.currentPlayer()).isEqualByComparingTo(Player.FIRST);
+        }
+
+        @Test
+        @DisplayName("two moves, one direction")
+        void twoMovesOneDirection() {
+            final var afterMoves = moveLog
+                    .addMove(new Move(List.of(Direction.NE)))
+                    .addMove(new Move(List.of(Direction.N)))
+                    .add(Direction.W);
+
+            Assertions.assertThat(afterMoves.currentPlayer()).isEqualByComparingTo(Player.FIRST);
+        }
+
+        @Test
+        @DisplayName("one moves, one direction, one move")
+        void oneMoveOneDirectionOneMove() {
+            final var afterMoves = moveLog
+                    .addMove(new Move(List.of(Direction.NE)))
+                    .add(Direction.W)
+                    .addMove(new Move(List.of(Direction.N)));
+
+            Assertions.assertThat(afterMoves.currentPlayer()).isEqualByComparingTo(Player.SECOND);
+        }
+
+        @Test
+        @DisplayName("three moves")
+        void threeMoves() {
+            final var afterMoves = moveLog
+                    .addMove(new Move(List.of(Direction.S)))
+                    .addMove(new Move(List.of(Direction.W)))
+                    .addMove(new Move(List.of(Direction.N)));
+
+            Assertions.assertThat(afterMoves.currentPlayer()).isEqualByComparingTo(Player.SECOND);
+        }
+
+        @Test
+        @DisplayName("one move, one undo")
+        void oneMoveOneUndo() {
+            final var afterMoves = moveLog
+                    .addMove(new Move(List.of(Direction.S)))
+                    .undoMove();
+
+            Assertions.assertThat(afterMoves.currentPlayer()).isEqualByComparingTo(Player.FIRST);
+        }
+
+        @Test
+        @DisplayName("one direction, one undo")
+        void oneDirectionOneUndo() {
+            final var afterMoves = moveLog
+                    .add(Direction.N)
+                    .undoMove();
+
+            Assertions.assertThat(afterMoves.currentPlayer()).isEqualByComparingTo(Player.FIRST);
+        }
+
+        @Test
+        @DisplayName("one move, one direction, one undo")
+        void oneMoveOneDirectionOneUndo() {
+            final var afterMoves = moveLog
+                    .addMove(new Move(List.of(Direction.S)))
+                    .add(Direction.N)
+                    .undoMove();
+
+            Assertions.assertThat(afterMoves.currentPlayer()).isEqualByComparingTo(Player.FIRST);
+        }
+
+        @Test
+        @DisplayName("two move, one undo")
+        void twoMovesOneUndo() {
+            final var afterMoves = moveLog
+                    .addMove(new Move(List.of(Direction.S)))
+                    .addMove(new Move(List.of(Direction.S)))
+                    .undoMove();
+
+            Assertions.assertThat(afterMoves.currentPlayer()).isEqualByComparingTo(Player.SECOND);
+        }
     }
 }
