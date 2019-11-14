890202a396661a756dcf7a94a0d6dd132596561d
robert <ostaszewski.robert@hotmail.com>
2019-11-14 07:35:48 +0100

    MR changes

--- src/main/java/com/github/lipinskipawel/board/ai/bruteforce/SmartBoardEvaluator.java
+++ src/main/java/com/github/lipinskipawel/board/ai/bruteforce/SmartBoardEvaluator.java
@@ -4,17 +4,23 @@ import com.github.lipinskipawel.board.ai.BoardEvaluator;
 import com.github.lipinskipawel.board.engine.BoardInterface;
 
 import java.util.List;
-import java.util.stream.IntStream;
 
-import static java.util.stream.Collectors.toList;
+public final class SmartBoardEvaluator implements BoardEvaluator {
 
-public class SmartBoardEvaluator implements BoardEvaluator {
-
-    private static int CONST = 10;
-    private static List<Integer> POINTS = IntStream.range(-7, 7)
-            .flatMap(i -> IntStream.range(0, 9).map(j -> i * CONST))
-            .boxed()
-            .collect(toList());
+    private static List<Integer> POINTS = List.of(
+            -60, -60, -60, -60, -60, -60, -60, -60, -60,
+            -50, -50, -50, -50, -50, -50, -50, -50, -50,
+            -40, -40, -40, -40, -40, -40, -40, -40, -40,
+            -30, -30, -30, -30, -30, -30, -30, -30, -30,
+            -20, -20, -20, -20, -20, -20, -20, -20, -20,
+            -10, -10, -10, -10, -10, -10, -10, -10, -10,
+              0,   0,   0,   0,   0,   0,   0,   0,   0,
+             10,  10,  10,  10,  10,  10,  10,  10,  10,
+             20,  20,  20,  20,  20,  20,  20,  20,  20,
+             30,  30,  30,  30,  30,  30,  30,  30,  30,
+             40,  40,  40,  40,  40,  40,  40,  40,  40,
+             50,  50,  50,  50,  50,  50,  50,  50,  50,
+             60,  60,  60,  60,  60,  60,  60,  60,  60);
 
     @Override
     public double evaluate(BoardInterface board) {
